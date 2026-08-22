fcf2938c91f5ced4ec07cbd88811ced0efa521e9
homepunks <wispenjoyer@gmail.com>
2026-08-22 14:47:26 +0500

    refactor: decrease memory usage

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,9 +1,9 @@
 use num_modular::ModularUnaryOps;
 
 pub const ALPHABET_SIZE: u8 = 26;
-const M: u32 = ALPHABET_SIZE as u32;
+const M: u16 = ALPHABET_SIZE as u16;
 
-pub fn encrypt(src: &[u8], a: u8, b: u8) -> anyhow::Result<Vec<u8>> {
+pub fn encrypt(src: &[u8], a: u8, b: u8) -> Vec<u8> {
     let mut cipher_bytes = Vec::new();
     for &c in src {
         if !c.is_ascii_alphabetic() {
@@ -11,15 +11,15 @@ pub fn encrypt(src: &[u8], a: u8, b: u8) -> anyhow::Result<Vec<u8>> {
             continue;
         }
         let base = if c.is_ascii_uppercase() { b'A' } else { b'a' };
-        let x = u32::from(c - base);
-        let enc = ((u32::from(a) * x + u32::from(b)) % M) as u8;
+        let x = u16::from(c - base);
+        let enc = ((u16::from(a) * x + u16::from(b)) % M) as u8;
         cipher_bytes.push(enc + base);
     }
 
-    Ok(cipher_bytes)
+    cipher_bytes
 }
 
-pub fn decrypt(src: &[u8], a_inv: u8, b: u8) -> anyhow::Result<Vec<u8>> {
+pub fn decrypt(src: &[u8], a_inv: u8, b: u8) -> Vec<u8> {
     let mut plain_bytes = Vec::new();
     for &c in src {
         if !c.is_ascii_alphabetic() {
@@ -27,12 +27,12 @@ pub fn decrypt(src: &[u8], a_inv: u8, b: u8) -> anyhow::Result<Vec<u8>> {
             continue;
         }
         let base = if c.is_ascii_uppercase() { b'A' } else { b'a' };
-        let x = u32::from(c - base);
-        let dec = (u32::from(a_inv) * ((x + M - u32::from(b) % M) % M) % M) as u8;
+        let x = u16::from(c - base);
+        let dec = (u16::from(a_inv) * ((x + M - u16::from(b) % M) % M) % M) as u8;
         plain_bytes.push(dec + base);
     }
 
-    Ok(plain_bytes)
+    plain_bytes
 }
 
 pub fn brute_force(src: &[u8], a_inv: u8, b: u8) -> anyhow::Result<Vec<Vec<u8>>> {
--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,9 @@ fn main() -> anyhow::Result<()> {
 
     let a = 7;
     let b = 25;
-    let cipher_bytes = encrypt(&data_bytes, a, b)?;
+    let cipher_bytes = encrypt(&data_bytes, a, b);
     if let Some(a_inv) = a.invm(&ALPHABET_SIZE) {
-        let plain_bytes = decrypt(&cipher_bytes, a_inv, b)?;
+        let plain_bytes = decrypt(&cipher_bytes, a_inv, b);
         assert_eq!(plain_bytes, data_bytes);
         println!("ROUND TRIP SUCCEEDED");
     } else {
